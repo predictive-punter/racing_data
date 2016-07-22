5505adcf5599603f37037689f0b8db76bdfb1a04
Jason Green <justjasongreen@gmail.com>
2016-07-22 15:16:57 +1000

    Implement tests.jockey_test.test_performances

--- racing_data/jockey.py
+++ racing_data/jockey.py
@@ -3,4 +3,9 @@ from . import Entity
 
 class Jockey(Entity):
     """A jockey represents the person riding a runner"""
-    pass
+    
+    @property
+    def performances(self):
+        """Return a list of performances associated with this jockey"""
+
+        return self.get_cached_property('performances', self.provider.get_performances_by_jockey, self)
