213f4132033262e51ad9b99f757ff3391f79f1b2
Jason Green <justjasongreen@gmail.com>
2016-07-25 20:05:29 +1000

    Implement tests.performance_list_test.test_seconds

--- racing_data/performance_list.py
+++ racing_data/performance_list.py
@@ -2,6 +2,12 @@ class PerformanceList(list):
     """A performance list provides statistical analysis functionality for a list of performances"""
     
     @property
+    def seconds(self):
+        """Return the number of second placed performances in this list"""
+        
+        return self.count_results(2)
+
+    @property
     def starts(self):
         """Return the number of starts in this performance list"""
 
@@ -11,7 +17,7 @@ class PerformanceList(list):
     def wins(self):
         """Return the number of winning performances in this list"""
 
-        return len([performance for performance in self if performance['result'] == 1])
+        return self.count_results(1)
 
     @property
     def win_pct(self):
@@ -19,3 +25,8 @@ class PerformanceList(list):
 
         if self.starts > 0:
             return self.wins / self.starts
+
+    def count_results(self, result):
+        """Count the number of performances with the specified result in this list"""
+
+        return len([performance for performance in self if performance['result'] == result])
--- tests/performance_list_test.py
+++ tests/performance_list_test.py
@@ -8,6 +8,18 @@ def performance_list(performances):
     return racing_data.PerformanceList(performances)
 
 
+def count_results(performances, result):
+    """Count the number of performances with the specified result"""
+
+    return len([performance for performance in performances if performance['result'] == result])
+
+
+def test_seconds(performance_list, performances):
+    """The seconds property should return the number of second placed performances in the list"""
+
+    assert performance_list.seconds == count_results(performances, 2)
+
+
 def test_starts(performance_list, performances):
     """The starts property should return the number of performances in the list"""
 
@@ -17,7 +29,7 @@ def test_starts(performance_list, performances):
 def test_wins(performance_list, performances):
     """The wins property should return the number of winning performances in the list"""
 
-    assert performance_list.wins == len([performance for performance in performances if performance['result'] == 1])
+    assert performance_list.wins == count_results(performances, 1)
 
 
 def test_win_pct(performance_list, performances):
