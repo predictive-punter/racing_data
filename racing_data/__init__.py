198e151f0f84aa0e641b0b7077eb75c2937b28f8
Jason Green <justjasongreen@gmail.com>
2016-07-29 13:31:31 +1000

    Bump version: 1.0.0b8 -> 1.0.0b9

--- racing_data/__init__.py
+++ racing_data/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.0b8'
+__version__ = '1.0.0b9'
 
 from .performance_list import PerformanceList
 from .entity import Entity
--- tests/racing_data_test.py
+++ tests/racing_data_test.py
@@ -4,4 +4,4 @@ import racing_data
 def test_version():
     """racing_data.__version__ should return the correct version string"""
 
-    assert racing_data.__version__ == '1.0.0b8'
+    assert racing_data.__version__ == '1.0.0b9'
